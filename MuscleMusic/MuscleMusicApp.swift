cbad25879fedcf0148ac1fb1fdb2ae7d29ed86b6
Ing. Ebu Celik <ebucelik1@hotmail.com>
2023-01-27 20:34:27 +0100

    Initial Commit

---
+++ MuscleMusic/MuscleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMusicApp.swift
+//  MuscleMusic
+//
+//  Created by Ing. Ebu Celik, BSc on 27.01.23.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
